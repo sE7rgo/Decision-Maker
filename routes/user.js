17b1e2d247aafd3b167b2f9f839576710599bfd8
Graham <lord_proton@yahoo.ca>
2020-07-27 00:15:53 +0000

    commented out this route as it is unnecessary

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 //********************** user routes for login/logout  **********************
 
+// ***************  this is not needed ***************************
+
 const express = require('express');
 const router  = express.Router()
 
-module.exports = (db) => {              //simple login functionality no security
+//module.exports = (db) => {              //simple login functionality no security
   router.get("/login", (req, res) => {
     db.query(`INSERT INTO questions () ;`) //we may need to simply include the login
       .then(data => {                     //as just another field like straw poll so
