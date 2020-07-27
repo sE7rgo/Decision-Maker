967854d5bfa7a030b086b96eb23779a13dac6deb
Graham <lord_proton@yahoo.ca>
2020-07-27 22:45:19 +0000

    this is the seeds for the voters table

---
+++ db/seeds/voters.sql
@@ -0,0 +1,18 @@
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (1, 1, 'anybody@anywhere.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (2, 1, 'somebody@somewhere.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (3, 1, 'nobody@nowhere.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (4, 2, 'body@where.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (5, 2, 'waterbody@waterwhere.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (6, 2, 'topbody@topwhere.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (7, 3, 'any@any.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (8, 3, 'terror@terrific.com');
+INSERT INTO voters (id, questions_id, voter_email)
+VALUES (9, 3, 'what@how.com');
