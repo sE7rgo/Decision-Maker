37ff1a4fa38b20ea3f78b9a79a95c081cf5798fa
Graham <lord_proton@yahoo.ca>
2020-07-29 00:30:45 +0000

    still debugging

---
+++ helper_functions/borda.js
@@ -0,0 +1,39 @@
+/*
+The Borda count is a preferential, or ranked, voting system; the voter ranks the list of candidates in order of preference. So, for example, the voter gives a 1 to their most preferred candidate, a 2 to their second most preferred, and so on.
+
+Points are then given to each candidate in reverse proportion to their ranking, so that higher-ranked candidates receive more points. When all votes have been counted, and the points added up, the candidate with the most points wins.
+
+Ranking	Candidate	Formula	Points	Relative points
+1st	    Andrew	    n	      5	      1.00
+2nd	    Brian	      n−1	    4	      0.80
+3rd	    Catherine	  n−2	    3	      0.60
+4th	    David	      n−3	    2	      0.40
+5th	    Elizabeth	  n−4	    1	      0.20
+Taken from:  https://en.wikipedia.org/wiki/Borda_count
+*/
+const borda = function(rankingsFromDB, voterRankings) {  //these are arrays of rankings
+  let count = voterRankings.length;
+  let pointsArray = voterRankings;
+  let finalArray = [];
+  if (rankingsFromDB[0] !== 0) {
+  for (let i = 1; i <= voterRankings.length; (i + 1)) {
+    voterRankings[voterRankings.indexOf(i)] += 1;
+    count --;
+  } console.log(voterRankings);
+  for (let j = 0; j < pointsArray.length; j++) {
+    finalArray[j] = pointsArray[j] + rankingsFromDB[j]
+  }
+  return finalArray;
+} else {
+      let initialRanking = voterRankings;
+      return initialRanking;
+    }
+
+};
+let bordaRankingDB = [1, 3, 2];
+let userRanking = [2, 1, 3];
+console.log(borda(bordaRankingDB, userRanking));
+
+
+
+module.exports = {borda};
