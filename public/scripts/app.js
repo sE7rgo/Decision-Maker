5cd1f1cdabba37b8bad245ea78a847173910f3b3
sE7rgo <se7rgo@gmail.com>
2020-07-30 19:08:01 +0000

    fixed options and emails errors

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 /* Create an option HTML */
 const createOption = (() => {
   const option = `<div class="option"></div>
-  <input type="text" name="options_1" placeholder="Add an Option"></input>
+  <input type="text" name="options[]" placeholder="another option"></input>
   </div>`
   return option;
 });
@@ -36,15 +36,26 @@ $(document).ready(() => {
         $('.error').slideDown(1000);
       } else {
       $('.new-option').siblings('output')
-      .val(`You can add ${optionCounter} more options`);
+      .val(`You can add ${optionCounter} more options`).css('color red');
       event.preventDefault();
       appendOption();
       }
     });
 
-
+    let emailCounter = 3;
+    $('.new-email').siblings('output')
+    .val(`You can add ${emailCounter} more emails`)
   $('.new-email').click((event) => {
-    event.preventDefault();
-    appendEmail();
+    emailCounter--;
+    if(emailCounter < 0) {
+      $('.error').text(`❌ That's it, only 5 emails 😀`);
+      $('.error').css('border', '3px solid red');
+      $('.error').slideDown(1000);
+    } else {
+      $('.new-email').siblings('output')
+      .val(`You can add ${emailCounter} more emails`)
+      event.preventDefault();
+      appendEmail();
+    }
   });
 })
