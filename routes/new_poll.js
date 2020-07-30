d7652cf68f1f66ddeac0c2a933ba8be6f229473f
sE7rgo <se7rgo@gmail.com>
2020-07-30 22:11:58 +0000

    added result page route in routes, css for result page and it's html

--- routes/new_poll.js
+++ routes/new_poll.js
@@ -5,6 +5,8 @@ const {mg} = require('../helper_functions/mailgun');
 const { generateRandomString } = require('../public/scripts/helpers');
 
 module.exports = (db) => {
+//***************************  POST new poll ************************************
+
   router.post("/poll/new", (req, res) => {
     const pollId = generateRandomString(6);
     const creator_email = req.session.user_id;
@@ -12,55 +14,97 @@ module.exports = (db) => {
     const values = [creator_email, question, pollId];
                         console.log(req.body);
     db.query (`
-    INSERT INTO questions
-    (creator_email, question_text, poll_code)
-    VALUES ($1, $2, $3)
-    RETURNING *
-    ;`, values)
-  .catch(err => {
-    console.log(err);
-    return null;
-  })
-  .then(res => {
-    return res.rows;
-  })
-  .then(() => {
-    return Promise.all(options.map(option => {
-     return db.query(`
-      INSERT INTO choices
-      (poll_code,choice_text)
-      VALUES ($1, $2)
+      INSERT INTO questions
+      (creator_email, question_text, poll_code)
+      VALUES ($1, $2, $3)
       RETURNING *
-      ;`, [pollId, option])
-      .then(res => {
-        return res.rows;
-      })
-      .catch(err => {
-        console.log(err);
-        return null;
-      })
-    }))
-  })
-  .then(() => {
-    return Promise.all(emails.map(user_email => {
-      return db.query (`
-        INSERT INTO voters
-        (poll_code, voter_email)
+      ;`, values)
+    .catch(err => {
+      console.log(err);
+      return null;
+    })
+    .then(res => {
+      return res.rows;
+    })
+    .then(() => {
+      return Promise.all(options.map(option => {
+       return db.query(`
+        INSERT INTO choices
+        (poll_code,choice_text)
         VALUES ($1, $2)
         RETURNING *
-        ;`, [pollId, user_email])
+        ;`, [pollId, option])
+        .then(res => { console.log(res.rows);
+          return res.rows;
+        })
         .catch(err => {
           console.log(err);
           return null;
         })
-        .then(res => {
-        return res.rows;
+      }))
+    })
+    .then(() => {
+      return Promise.all(emails.map(user_email => {
+        return db.query (`
+          INSERT INTO voters
+          (poll_code, voter_email)
+          VALUES ($1, $2)
+          RETURNING *
+          ;`, [pollId, user_email])
+          .catch(err => {
+            console.log(err);
+            return null;
+          })
+          .then(res => {
+          return res.rows;
+          })
+      }))
+    })
+    .then(()=> {
+      let query = {
+        text: `SELECT questions.creator_email, questions.poll_code, voters.voter_email FROM questions
+        JOIN voters ON voters.poll_code = questions.poll_code
+        WHERE voters.poll_code = $1`,
+        values: [pollId]
+      };
+      //console.log(query);
+      let voterEmails = [];
+      db.query(query)
+        .then(data => {
+          console.log({data});
+          const poll_id = data.rows[0].poll_code;
+          const creatorEmail = data.rows[0].creator_email;
+          for (const row of data.rows) {
+            voterEmails.push(row.voter_email);
+          }
+          let insertVoters = voterEmails.toString().replace(/,/g, ', ');
+
+          // Mailgun Sendout to Users and Creator  ${creatorEmail}, ${insertVoters}
+          const inputData = {
+            from: 'Decision Maker<graham.l.tyler@gmail.com>',
+            to: `${creatorEmail}, ${insertVoters}`,
+            subject: 'Decision-Maker Poll',
+            text: `Clicking this link will take you to Decision Maker where ${creatorEmail} has posted a poll that they want you vote on. \n http://localhost:8080/poll/${poll_id}`
+          };
+
+          mg.messages().send(inputData, function(err, body) {
+            if (err) {
+              console.log("got an error: ", err);
+            } else {
+              console.log(body);
+      res.redirect(`/`);
+            }
+          });
         })
-    }))
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+      })
+    .then(()=> {
+      res.redirect(`/pollResults/${pollId}`);
+    })
   })
-  .then(()=> {
-    res.redirect(`/pollResults/${pollId}`);
-  })
-})
-return router;
+  return router;
 }
---
+++ routes/poll_retrieve.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const questions = require('./questions');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+  router.get("/pollResults/:pollId", (req, res) => {  //each vote cast, sends an update
+    let query = {
+      text: `SELECT questions.creator_email, questions.poll_code, questions.question_text, choices.choice_text, choices.borda_rank
+      FROM questions
+      JOIN choices ON choices.poll_code = questions.poll_code
+      WHERE questions.poll_code = $1`,
+      values: [req.params.pollId]
+    };
+    let resultsTally = [];
+    db.query(query)
+      .then(data => {
+        const creatorEmail = data.rows[0].creator_email;
+        const poll_id = data.rows[0].poll_code;
+        const questionText = data.rows[0].question_text;
+
+        const bordaRank = data.rows.map(({borda_rank}) => borda_rank);
+        const pollOptions = data.rows.map(({choice_text}) => choice_text);
+
+        let options = {};
+
+        for (let j = 0; j < bordaRank.length; j++) {
+          // resultsTally.push(pollOptions[j]);
+          // resultsTally.push(bordaRank[j]);
+          options[pollOptions[j]] = {rank: bordaRank[j]}
+        }
+        let results = resultsTally.toString();
+        console.log(options)
+        // // Mailgun Sendout to Users and Creator  //re-install ${creatorEmail}
+        // const inputData = {
+        //   from: 'Decision Maker<graham.l.tyler@gmail.com>',
+        //   to: `lord_proton@yahoo.ca`,
+        //   subject: 'Decision-Maker Poll',
+        //   text: `Current Poll Results for Poll Code: ${poll_id} \n Given by: ${creatorEmail} \n Asked: ${questionText} \n Decision Maker results: ${results}`
+        // };
+
+        // mg.messages().send(inputData, function(err, body) {
+        //   if (err) {
+        //     console.log("got an error: ", err);
+        //   } else {
+        //     console.log(body);
+        //   }
+        // });
+
+        let templateVars = {
+          pollId: req.params.pollId,
+          question: questionText,
+          choices: options
+        };
+        res.render("poll_result", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
