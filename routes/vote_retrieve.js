b83c247356a13887391d60b96c08e985b656e960
Graham <lord_proton@yahoo.ca>
2020-07-30 03:06:44 +0000

    pasted in get/pollResults/:id to bottom .then

--- routes/vote_retrieve.js
+++ routes/vote_retrieve.js
@@ -34,9 +34,57 @@ module.exports = (db) => {
           WHERE poll_code = $2 AND choice_text = $3
           ;`, [updatedPoints, poll_code, option])
         })
-        Promise.all(promises)
-        .then(() => res.send('successful update'));
-     });
-  });
+        Promise.all(promises)         //() => res.send('successful update'));
+        .then(()=> {  //****************spliced in fm questions.js *************
+          let query = {
+            text: `SELECT questions.creator_email, questions.poll_code, questions.question_text, choices.choice_text, choices.borda_rank
+            FROM questions
+            JOIN choices ON choices.poll_code = questions.poll_code
+            WHERE questions.poll_code = $1`,
+            values: [req.params.id]
+          };
+          //console.log(query);
+          let bordaRank = [];
+          let pollOptions = [];
+          let resultsTally = [];
+          db.query(query)
+            .then(data => {
+              const creatorEmail = data.rows[0].creator_email;
+              const poll_id = data.rows[0].poll_code;
+              const questionText = data.rows[0].question_text;
+              for (const row of data.rows) {
+                bordaRank.push(row.borda_rank);
+                pollOptions.push(row.choice_text);
+              }
+              for (let j = 0; j < bordaRank.length; j++) {
+                resultsTally.push(pollOptions[j]);
+                resultsTally.push(bordaRank[j]);
+              }
+              let results = resultsTally.toString();
+              //console.log(results);
+              // Mailgun Sendout to Users and Creator  //re-install ${creatorEmail}
+              const inputData = {
+                from: 'Decision Maker<graham.l.tyler@gmail.com>',
+                to: `lord_proton@yahoo.ca`,
+                subject: 'Decision-Maker Poll',
+                text: `Current Poll Results for Poll Code: ${poll_id} \n Given by: ${creatorEmail} \n Asked: ${questionText} \n Decision Maker results: ${results}`
+              };
+
+              mg.messages().send(inputData, function(err, body) {
+                if (err) {
+                  console.log("got an error: ", err);
+                } else {
+                  console.log(body);
+                }
+              });
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+        })
+      });
+  })
   return router;
 };
