2bf1bf3ad62d9f3c77e43576435dc646776ddaf8
Graham <lord_proton@yahoo.ca>
2020-07-31 14:37:34 +0000

    cleaned up dead code and removed comments

--- routes/vote_retrieve.js
+++ routes/vote_retrieve.js
@@ -11,10 +11,7 @@ module.exports = (db) => {
     console.log('this is reqbody.........',req.body.borda_rank);
     const { options, poll_code, borda_rank } = req.body;
     let newChoices = req.body.borda_rank;
-    let newRankings = [];
     let oldRankings = [];
-    let oldChoices = [];
-    let updatedPoints = [];
 
     let query = {
       text: `SELECT borda_rank, choice_text
@@ -25,87 +22,79 @@ module.exports = (db) => {
     db.query(query)               //initiate query to fetch old rankings fm DB
       .then(data => {
         for (const row of data.rows) {
-         // oldRankings.push(row.borda_rank);
           oldRankings.push({choice_text: row.choice_text,
-          borda_rank: row.borda_rank});
-          //oldChoices.push(row.choice_text);
+            borda_rank: row.borda_rank});
         }
-        //for (const choice of oldChoices) {
-         // newRankings.push(newChoices.indexOf(choice) + 1);
-
-        //}console.log(newRankings);
 
         for (const index in newChoices) {
-          const x = oldRankings.find(item => item.choice_text === newChoices[index])
+          const x = oldRankings.find(item => item.choice_text === newChoices[index]);
           x.borda_rank = x.borda_rank + (newChoices.length - index);
         }
 
-       // updatedPoints = borda(oldRankings, newRankings); //call borda method
-
-        const promises = oldRankings.map((option) => {  //conduct update rankings in DB
+        const promises = oldRankings.map((option) => {
           return db.query(`
           UPDATE choices
           SET borda_rank = $1
           WHERE poll_code = $2 AND choice_text = $3
-          ;`, [option.borda_rank, poll_code, option.choice_text])
-        })
+          ;`, [option.borda_rank, poll_code, option.choice_text]);
+        });
         Promise.all(promises)
 
-        .then(()=> {
-          let query = {
-            text: `SELECT questions.creator_email, questions.poll_code, questions.question_text, choices.choice_text, choices.borda_rank
+          .then(()=> {
+            let query = {
+              text: `SELECT questions.creator_email, questions.poll_code, questions.question_text, choices.choice_text, choices.borda_rank
             FROM questions
             JOIN choices ON choices.poll_code = questions.poll_code
             WHERE questions.poll_code = $1`,
-            values: [poll_code]
-          };
-          //console.log(query);
-          let bordaRank = [];
-          let pollOptions = [];
-          let resultsTally = [];
-          db.query(query)
-            .then(data => {
-              const creatorEmail = data.rows[0].creator_email;
-              const poll_id = data.rows[0].poll_code;
-              const questionText = data.rows[0].question_text;
-              for (const row of data.rows) {
-                bordaRank.push(row.borda_rank);
-                pollOptions.push(row.choice_text);
-              }
-              for (let j = 0; j < bordaRank.length; j++) {
-                resultsTally.push(pollOptions[j]);
-                resultsTally.push(bordaRank[j]);
-              }
-              let results = resultsTally.toString();
-              //console.log(results);
-              // Mailgun Sendout to Users and Creator  //re-install ${creatorEmail}
-              const inputData = {
-                from: 'Decision Maker<graham.l.tyler@gmail.com>',
-                to: `${creatorEmail}`,
-                subject: 'Decision-Maker Poll',
-                text: `Current Poll Results for Poll Code: ${poll_id} \n Given by: ${creatorEmail} \n Asked: ${questionText} \n Decision Maker results: ${results}`
-              };
+              values: [poll_code]
+            };
 
-              mg.messages().send(inputData, function(err, body) {
-                if (err) {
-                  console.log("got an error: ", err);
-                } else {
-                  console.log(body);
+            let bordaRank = [];
+            let pollOptions = [];
+            let resultsTally = [];
+            db.query(query)
+              .then(data => {
+                const creatorEmail = data.rows[0].creator_email;
+                const poll_id = data.rows[0].poll_code;
+                const questionText = data.rows[0].question_text;
+                for (const row of data.rows) {
+                  bordaRank.push(row.borda_rank);
+                  pollOptions.push(row.choice_text);
                 }
-              });
-            })
+                for (let j = 0; j < bordaRank.length; j++) {
+                  resultsTally.push(pollOptions[j]);
+                  resultsTally.push(bordaRank[j]);
+                }
+                let results = resultsTally.toString();
 
-            .then(()=> {
-              res.redirect(`/pollResults/${poll_code}`);
-            })
+                // Mailgun Sendout to Users and Creator  //re-install ${creatorEmail}
+                const inputData = {
+                  from: 'Decision Maker<graham.l.tyler@gmail.com>',
+                  to: `${creatorEmail}`,
+                  subject: 'Decision-Maker Poll',
+                  text: `Current Poll Results for Poll Code: ${poll_id} \n Given by: ${creatorEmail} \n Asked: ${questionText} \n Decision Maker results: ${results}`
+                };
+
+                mg.messages().send(inputData, function(err, body) {
+                  if (err) {
+                    console.log("got an error: ", err);
+                  } else {
+                    console.log(body);
+                  }
+                });
+              })
+
+              .then(()=> {
+                res.redirect(`/pollResults/${poll_code}`);
+              });
           })
-            .catch(err => {
-              res
-                .status(500)
-                .json({ error: err.message });
-            });
-        })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
       });
+  });
 
   return router;
 };
